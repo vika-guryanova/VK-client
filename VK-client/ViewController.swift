1f2c950d8ca8870fafa43572cd0e19b7fe47a04b
Victoria Guryanova <guryanovavv@gmail.com>
2021-07-05 18:29:43 +0300

    Initial Commit

---
+++ VK-client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK-client
+//
+//  Created by Victoria Guryanova on 05.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
